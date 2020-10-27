a073714c6e278e34cee7cb21e8a5e1ad00c394f8
Michael Heiniger <michael.heiniger89@protonmail.ch>
2020-10-27 19:47:33 +0100

    Improve readability

--- app/src/main/java/ch/qscqlmpa/dwitch/ongoinggame/communication/websocket/WebsocketModule.kt
+++ app/src/main/java/ch/qscqlmpa/dwitch/ongoinggame/communication/websocket/WebsocketModule.kt
@@ -1,10 +1,11 @@
 package ch.qscqlmpa.dwitch.ongoinggame.communication.websocket
 
+import ch.qscqlmpa.dwitch.components.ongoinggame.OnGoingGameQualifiers.HOST_IP_ADDRESS
+import ch.qscqlmpa.dwitch.components.ongoinggame.OnGoingGameQualifiers.HOST_PORT
 import ch.qscqlmpa.dwitch.ongoinggame.communication.websocket.client.ProdWebsocketClient
 import ch.qscqlmpa.dwitch.ongoinggame.communication.websocket.client.WebsocketClient
 import ch.qscqlmpa.dwitch.ongoinggame.communication.websocket.server.ProdWebsocketServer
 import ch.qscqlmpa.dwitch.ongoinggame.communication.websocket.server.WebsocketServer
-import ch.qscqlmpa.dwitch.components.ongoinggame.OnGoingGameQualifiers
 import ch.qscqlmpa.dwitch.service.OngoingGameScope
 import dagger.Module
 import dagger.Provides
@@ -20,8 +21,8 @@ class WebsocketModule {
         @JvmStatic
         @Provides
         fun bindWebsocketServer(
-                @Named(OnGoingGameQualifiers.HOST_IP_ADDRESS) hostIpAddress: String,
-                @Named(OnGoingGameQualifiers.HOST_PORT) hostPort: Int
+                @Named(HOST_IP_ADDRESS) hostIpAddress: String,
+                @Named(HOST_PORT) hostPort: Int
         ): WebsocketServer {
             return ProdWebsocketServer(hostIpAddress, hostPort)
         }
@@ -30,8 +31,8 @@ class WebsocketModule {
         @JvmStatic
         @Provides
         fun bindWebsocketClient(
-                @Named(OnGoingGameQualifiers.HOST_IP_ADDRESS) hostIpAddress: String,
-                @Named(OnGoingGameQualifiers.HOST_PORT) hostPort: Int
+                @Named(HOST_IP_ADDRESS) hostIpAddress: String,
+                @Named(HOST_PORT) hostPort: Int
         ): WebsocketClient {
             return ProdWebsocketClient(hostIpAddress, hostPort)
         }
