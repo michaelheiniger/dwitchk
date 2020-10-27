012c7791b4b6144a7ce78de30e94a8e98ca0fad4
Michael Heiniger <michael.heiniger89@protonmail.ch>
2020-10-27 20:15:27 +0100

    Start HostInGameService/GuestInGameService only when game persistence is complete
    In addition: hard-code destination port of game to join

--- app/src/main/java/ch/qscqlmpa/dwitch/usecases/NewGameUsecase.kt
+++ app/src/main/java/ch/qscqlmpa/dwitch/usecases/NewGameUsecase.kt
@@ -4,35 +4,51 @@ import ch.qscqlmpa.dwitch.gamediscovery.AdvertisedGame
 import ch.qscqlmpa.dwitch.ongoinggame.services.ServiceManager
 import ch.qscqlmpa.dwitch.persistence.Store
 import io.reactivex.Completable
+import io.reactivex.Single
 import javax.inject.Inject
 
 class NewGameUsecase @Inject
-constructor(private val serviceManager: ServiceManager,
-            private val store: Store
+constructor(
+    private val serviceManager: ServiceManager,
+    private val store: Store
 ) {
 
     fun hostNewgame(gameName: String, playerName: String): Completable {
-        return Completable.fromAction {
-            val insertGameResult = store.insertGameForHost(gameName, playerName, "127.0.0.1", 8889) //TODO Set correct port
-            serviceManager.startHostService(insertGameResult.gameLocalId, insertGameResult.localPlayerLocalId)
+        return Single.fromCallable {
+            store.insertGameForHost(
+                gameName,
+                playerName,
+                "127.0.0.1",
+                8889 //TODO Set correct port
+            )
         }
+            .doAfterSuccess { insertGameResult ->
+                serviceManager.startHostService(
+                    insertGameResult.gameLocalId,
+                    insertGameResult.localPlayerLocalId
+                )
+            }
+            .ignoreElement()
     }
 
     fun joinGame(advertisedGame: AdvertisedGame, playerName: String): Completable {
-        return Completable.fromAction {
-            val insertGameResult = store.insertGameForGuest(
-                    advertisedGame.name,
-                    playerName,
-                    advertisedGame.ipAddress,
-                    advertisedGame.port
-            )
-
-            serviceManager.startGuestService(
-                    insertGameResult.gameLocalId,
-                    insertGameResult.localPlayerLocalId,
-                    advertisedGame.port,
-                    advertisedGame.ipAddress
+        return Single.fromCallable {
+            store.insertGameForGuest(
+                advertisedGame.name,
+                playerName,
+                advertisedGame.ipAddress,
+                advertisedGame.port
             )
         }
+            .doAfterSuccess { insertGameResult ->
+                serviceManager.startGuestService(
+                    insertGameResult.gameLocalId,
+                    insertGameResult.localPlayerLocalId,
+//                    advertisedGame.port, //FIXME: it isn't the same port as the one advertised...
+                    8889,
+                    advertisedGame.ipAddress
+                )
+            }
+            .ignoreElement()
     }
 }
