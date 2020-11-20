2c2c2e1625ed23446768dbc0f20acb459f36091c
Michael Heiniger <michael.heiniger89@protonmail.ch>
2020-11-20 15:34:08 +0100

    Reformat code

--- app/src/main/java/ch/qscqlmpa/dwitch/ongoinggame/gameevent/GuestGameEventRepository.kt
+++ app/src/main/java/ch/qscqlmpa/dwitch/ongoinggame/gameevent/GuestGameEventRepository.kt
@@ -1,9 +1,8 @@
 package ch.qscqlmpa.dwitch.ongoinggame.gameevent
 
-import ch.qscqlmpa.dwitch.ongoinggame.events.EventRepository
 import ch.qscqlmpa.dwitch.ongoinggame.OngoingGameScope
+import ch.qscqlmpa.dwitch.ongoinggame.events.EventRepository
 import javax.inject.Inject
 
 @OngoingGameScope
-internal class GuestGameEventRepository @Inject
-constructor() : EventRepository<GuestGameEvent>()
+internal class GuestGameEventRepository @Inject constructor() : EventRepository<GuestGameEvent>()
