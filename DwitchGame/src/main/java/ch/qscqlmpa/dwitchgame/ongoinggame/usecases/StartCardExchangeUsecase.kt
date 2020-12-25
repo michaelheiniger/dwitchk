4d0f4205ebb6d4945fcd3fede7b68e4b702d4ecd
Michael Heiniger <michael.heiniger89@protonmail.ch>
2020-12-25 10:58:19 +0100

    Add test for StartCardExchangeUsecase

--- DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/usecases/StartCardExchangeUsecase.kt
+++ DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/usecases/StartCardExchangeUsecase.kt
@@ -6,14 +6,16 @@ import ch.qscqlmpa.dwitchengine.model.game.CardExchange
 import ch.qscqlmpa.dwitchengine.model.game.GameState
 import ch.qscqlmpa.dwitchgame.ongoinggame.communication.host.HostCommunicator
 import ch.qscqlmpa.dwitchgame.ongoinggame.communication.messagefactories.HostMessageFactory
+import ch.qscqlmpa.dwitchgame.ongoinggame.game.DwitchEngineFactory
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import timber.log.Timber
 import javax.inject.Inject
 
-class StartCardExchangeUsecase @Inject constructor(
+internal class StartCardExchangeUsecase @Inject constructor(
     private val communicator: HostCommunicator,
-    private val connectionStore: ConnectionStore
+    private val connectionStore: ConnectionStore,
+    private val dwitchEngineFactory: DwitchEngineFactory
 ) {
 
     fun startCardExchange(gameState: GameState): Completable {
@@ -24,13 +26,14 @@ class StartCardExchangeUsecase @Inject constructor(
                 if (connectionId != null) {
                     communicator.sendMessage(HostMessageFactory.createCardExchangeMessage(cardExchange, connectionId))
                 } else {
-                    Timber.e("No connection ID found in store for inGameId $playerId") //TODO: handle case where the message cannot be send
+                    //TODO: handle case where the message cannot be send
+                    Timber.e("No connection ID found in store for in-game ID: $playerId")
                     Completable.complete()
                 }
             }
     }
 
     private fun getCardExchanges(gameState: GameState): Observable<CardExchange> {
-        return Observable.fromIterable(DwitchEngine(gameState).getCardsExchanges())
+        return Observable.fromIterable(dwitchEngineFactory.create(gameState).getCardsExchanges())
     }
 }
