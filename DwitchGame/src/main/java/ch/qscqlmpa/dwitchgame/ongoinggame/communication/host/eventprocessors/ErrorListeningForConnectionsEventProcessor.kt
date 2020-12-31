77aac2c5652b7c075badda07bb576c8a1fc3b3c0
Michael Heiniger <michael.heiniger89@protonmail.ch>
2020-12-31 15:42:26 +0100

    Notify of ErrorListeningForConnections events so that the UI can react accordingly

--- DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/communication/host/eventprocessors/ErrorListeningForConnectionsEventProcessor.kt
+++ DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/communication/host/eventprocessors/ErrorListeningForConnectionsEventProcessor.kt
@@ -1,17 +1,23 @@
 package ch.qscqlmpa.dwitchgame.ongoinggame.communication.host.eventprocessors
 
 import ch.qscqlmpa.dwitchcommunication.websocket.server.ServerCommunicationEvent
+import ch.qscqlmpa.dwitchgame.ongoinggame.communication.host.HostCommunicationState
+import ch.qscqlmpa.dwitchgame.ongoinggame.communication.host.HostCommunicationStateRepository
 import io.reactivex.rxjava3.core.Completable
 import timber.log.Timber
 import javax.inject.Inject
 
 internal class ErrorListeningForConnectionsEventProcessor @Inject constructor(
-
+    private val communicationStateRepository: HostCommunicationStateRepository
 ) : HostCommunicationEventProcessor {
 
     override fun process(event: ServerCommunicationEvent): Completable {
+
         event as ServerCommunicationEvent.ErrorListeningForConnections
-        Timber.e("Error listening for connections: ${event.exception}")
-        return Completable.complete() // Nothing to do
+
+        return Completable.fromAction {
+            Timber.e("Error listening for connections: ${event.exception}")
+            communicationStateRepository.notify(HostCommunicationState.Error)
+        }
     }
 }
