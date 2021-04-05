b4060b1a08889f1cd1f8fd73b1a56d6c97cf59d6
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-04-05 23:27:10 +0200

    Remove unnecessary log

--- DwitchStore/src/main/java/ch/qscqlmpa/dwitchstore/ingamestore/InGameStoreImpl.kt
+++ DwitchStore/src/main/java/ch/qscqlmpa/dwitchstore/ingamestore/InGameStoreImpl.kt
@@ -134,7 +134,6 @@ internal class InGameStoreImpl constructor(
 
     override fun observeLocalPlayer(): Observable<Player> {
         return playerDao.observePlayer(localPlayerLocalId)
-            .doOnNext { player -> println("-->local player: $player") }
     }
 
     override fun getLocalPlayerDwitchId(): PlayerDwitchId {
