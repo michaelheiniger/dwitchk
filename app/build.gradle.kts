1afee52a474f0282ab70e2bd081c6f9f4d2e9159
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-04-14 21:29:04 +0200

    Upgrade dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -145,11 +145,11 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
     implementation("androidx.compose.runtime:runtime-rxjava3:$composeVersion")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-beta03")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-beta04")
     androidTestImplementation("androidx.compose.ui:ui-test:$composeVersion")
 
-    implementation("androidx.activity:activity-compose:1.3.0-alpha05")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04")
 
     // Robolectric (for unit tests that log stuff)
     testImplementation("org.robolectric:robolectric:4.5.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha14")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlinVersion}")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlintGradlePluginVersion}")
