7fcf64a4a0db7c5fd666ce271e01f28ab712151a
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-04-17 22:10:23 +0200

    Move PlayerWrUi to WaitingRoomFacade.kt

--- DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/waitingroom/WaitingRoomFacade.kt
+++ DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/waitingroom/WaitingRoomFacade.kt
@@ -1,8 +1,11 @@
 package ch.qscqlmpa.dwitchgame.ongoinggame.waitingroom
 
+import ch.qscqlmpa.dwitchmodel.player.PlayerConnectionState
 import io.reactivex.rxjava3.core.Observable
 
 interface WaitingRoomFacade {
 
     fun observePlayers(): Observable<List<PlayerWrUi>>
 }
+
+data class PlayerWrUi(val name: String, val connectionState: PlayerConnectionState, val ready: Boolean)
--- DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/ongoinggame/waitingroom/WrUiEntities.kt
+++
@@ -1,5 +0,0 @@
-package ch.qscqlmpa.dwitchgame.ongoinggame.waitingroom
-
-import ch.qscqlmpa.dwitchmodel.player.PlayerConnectionState
-
-data class PlayerWrUi(val name: String, val connectionState: PlayerConnectionState, val ready: Boolean)
