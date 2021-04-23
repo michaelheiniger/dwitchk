3bf729757d8b302f7c45e176b7a8e1c7f34fa449
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-04-23 22:29:42 +0200

    Replace unnecessary Single by Completable

--- DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/home/usecases/NewGameUsecase.kt
+++ DwitchGame/src/main/java/ch/qscqlmpa/dwitchgame/home/usecases/NewGameUsecase.kt
@@ -7,7 +7,6 @@ import ch.qscqlmpa.dwitchgame.appevent.GameJoinedInfo
 import ch.qscqlmpa.dwitchgame.gamediscovery.AdvertisedGame
 import ch.qscqlmpa.dwitchstore.store.Store
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class NewGameUsecase @Inject constructor(
@@ -16,16 +15,16 @@ class NewGameUsecase @Inject constructor(
 ) {
 
     fun hostGame(gameName: String, playerName: String, gamePort: Int): Completable {
-        return Single.fromCallable {
+        return Completable.fromAction {
             val result = store.insertGameForHost(gameName, playerName)
             appEventRepository.notify(AppEvent.GameCreated(GameCreatedInfo(result, gamePort)))
-        }.ignoreElement()
+        }
     }
 
     fun joinGame(advertisedGame: AdvertisedGame, playerName: String): Completable {
-        return Single.fromCallable {
+        return Completable.fromAction {
             val result = store.insertGameForGuest(advertisedGame.gameName, advertisedGame.gameCommonId, playerName)
             appEventRepository.notify(AppEvent.GameJoined(GameJoinedInfo(result, advertisedGame)))
-        }.ignoreElement()
+        }
     }
 }
