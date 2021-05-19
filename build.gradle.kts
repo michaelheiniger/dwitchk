9d6c3f73410433e6537d073613c563f0bc5ddcb4
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-05-19 17:36:34 +0200

    Remove commented-out ktlint code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-//    id("org.jlleitschuh.gradle.ktlint") version Versions.ktlintGradlePluginVersion
-}
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -14,7 +10,6 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlinVersion}")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
-//        classpath("org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlintGradlePluginVersion}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -27,14 +22,8 @@ allprojects {
         mavenCentral()
         jcenter()
     }
-//    apply(plugin = ("org.jlleitschuh.gradle.ktlint"))
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
-
-//ktlint {
-//    debug.set(true)
-//    verbose.set(true)
-//}
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,8 +5,6 @@ object Versions {
 
     const val kotlinVersion = "1.5.0"
 
-    const val ktlintGradlePluginVersion = "10.0.0"
-
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.2" // Keep in sync with ANDROID_BUILD_TOOLS_VERSION in .travis.yml
     const val minSdkVersion = 26
