6693d4b06b3858d1739c17c9bdbb79fe4082aa04
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-08-01 14:38:34 +0200

    Update androidx.appcompat:appcompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}")
 
     // Android
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
 
