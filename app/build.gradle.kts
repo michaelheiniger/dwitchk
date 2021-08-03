3f9ad178c00a2f60beb6838f11082cd1a379a89a
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-08-03 16:19:05 +0200

    Downgrade version of navigation-compose to alpha04 from alpha05
    See https://issuetracker.google.com/issues/194301889

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
     implementation("androidx.compose.runtime:runtime-rxjava3:$composeVersion")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
     implementation("androidx.activity:activity-compose:1.3.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
