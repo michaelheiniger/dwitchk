15c4cb967e777df88b751f24276d588062de124b
Michael Heiniger <michael.heiniger89@protonmail.ch>
2021-10-12 12:10:48 +0200

    Upgrade Android Gradle plugin to 7.0.3
    Update recommended when updating to Android Studio ArcticFox 2020.3.1 Patch 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlinVersion}")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
